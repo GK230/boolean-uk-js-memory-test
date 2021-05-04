8aeb2ae1c4524646602b670a190cce892c493b6a
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-05-04 16:00:32 +0100

    completed challenges

--- memory.js
+++ memory.js
@@ -1,29 +1,49 @@
-const randomNumbers = []
-const guessedNumbers = []
-const correctGuesses = []
-let correct = 0;
+function playMemoryTest(numberOfNumbers) {
 
-for (let i = 0; i < 4; i++) {
-    randomNumbers.push(Math.floor(Math.random() * 100) + 1);
-}
+    const randomNumbers = []
+    const guessedNumbers = []
+    const correctGuesses = []
+    let correct = 0;
 
-for (number of randomNumbers) {
-    alert(number)
-}
+    for (let i = 0; i < numberOfNumbers; i++) {
+        randomNumbers.push(Math.floor(Math.random() * 100) + 1);
+    }
 
-setTimeout(function() {
     for (number of randomNumbers) {
-        let guessedNumber = Number(prompt("What was the number?"))
-        guessedNumbers.push(guessedNumber)
+        alert(number)
     }
 
-    for (let i = 0; i < 4; i++) {
-        if (randomNumbers[i] === guessedNumbers[i]) {
-            correctGuesses.push(guessedNumbers[i])
+    setTimeout(function() {
+        for (let i = 0; i < numberOfNumbers + 1; i++) {
+            if (i = 1) {
+                let guessedNumber = Number(prompt("Enter the 1st number?"))
+                guessedNumbers.push(guessedNumber)
+            }
+
+            if (i = 2) {
+                let guessedNumber = Number(prompt("Enter the 2nd number?"))
+                guessedNumbers.push(guessedNumber)
+            } 
+
+            if (i = 3) {
+                let guessedNumber = Number(prompt("Enter the 3rd number?"))
+                guessedNumbers.push(guessedNumber)
+            } 
+
+            if (i >= 4) {
+                let guessedNumber = Number(prompt(`Enter the ${i}th number?`))
+                guessedNumbers.push(guessedNumber)
+            } 
         }
-    }
-    correct = correctGuesses.length;
+        
+        for (let i = 0; i < numberOfNumbers; i++) {
+            if (randomNumbers[i] === guessedNumbers[i]) {
+                correctGuesses.push(guessedNumbers[i])
+            }
+        }
+        correct = correctGuesses.length;
 
-    alert(`You got ${correct} right!`)
-}, 3000);
+        alert(`You got ${correct} right!`)
+    }, 3000);
 
+}
